07517661aed6a1b679db1c77d4eca9ec8d140ace
Vinicius de Paula <tqi_vpaula@uolinc.com>
2020-04-15 22:54:53 -0300

    ajustes

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -15,8 +15,8 @@ module.exports = {
    host: "ec2-3-82-12-16.compute-1.amazonaws.com",
    key: "~/.ssh/kp-nuvoli-inngage.pem",
    ref: "origin/master",
-   repo: "git@bitbucket.org:inngage/inn-node-api.git",
-   path: "/app/inn-node-api",
+   repo: "https://viniciusdepaula:v1n1c1us@bitbucket.org/inngage/inn-node-api.git",
+   path: "/home/ubuntu/app/inn-node-api",
    "post-deploy":
     "npm install && pm2 startOrRestart ecosystem.config.js"
   }
