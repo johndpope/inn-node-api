3dc01c826310f2e085677337e6cb973b6344f82e
Mohamed Ali <naptoon.ahmed@gmail.com>
2020-05-15 16:27:20 -0300

    Adding Cors Module

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.use(bodyParser.json());
 
 app.use((req,res,next) => {
     res.header('Access-Control-Allow-Origin','*');
-    res.header('Access-Control-Allow-Headers',true);
+    res.header('Access-Control-Allow-Headers','Origin, X-Requested-With, Content-Type, Accept');
     if(req.method === 'OPTIONS')
     {
         res.header('Access-Control-Allow-Methods', 'POST,GET');
