d5b7292b4caf2e85f9f63ab7bf5ada35d04877fe
Ubuntu <ubuntu@ip-172-31-48-142.ec2.internal>
2020-05-16 18:25:57 +0000

    ajustes porta 8080

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const http = require('http');
 const app = require('./app');
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 
 const server = http.createServer(app);
 
-server.listen(port);
+server.listen(port, "0.0.0.0");
+console.log('Server running at http://0.0.0.0:8080/');
