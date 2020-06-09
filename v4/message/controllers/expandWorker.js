61c473f2144aaf69a7e8073faa9457d5721a06c5
Mohamed Ali <naptoon.ahmed@gmail.com>
2020-06-09 16:30:44 -0300

    balancing the endpoints in with simple way

--- v4/message/controllers/expandWorker.js
+++ v4/message/controllers/expandWorker.js
@@ -3,12 +3,13 @@ const axios = require('axios');
 const router = express.Router();
 var con = require('../connection/DBconnection');
 
-const P2cBalancer = require('load-balancers');
 const endpoints = [
-    'http://ec2-54-166-246-71.compute-1.amazonaws.com:8080/api/message/',
-    'http://ec2-3-95-151-234.compute-1.amazonaws.com:8080/api/message/'
-];
-const balancer = new P2cBalancer(endpoints.length);
+        'http://ec2-54-166-246-71.compute-1.amazonaws.com:8080/api/message/',
+        'http://ec2-3-95-151-234.compute-1.amazonaws.com:8080/api/message/'
+    ];
+
+
+
 router.post('/v1',(req,res0,next)=>{
 
     con.getConnection(function(err99,connection){
@@ -364,7 +365,6 @@ router.post('/v3',(req,ress,next)=>{
     })
 })
 
-
 router.post('/v33',async(req,res,next)=>{
 
     con.getConnection(async function(err,connection){
@@ -425,7 +425,7 @@ router.post('/v33',async(req,res,next)=>{
                 console.log("sendPushRequest JSON")
                 console.log(sendPushRequest);
                 console.log("["+getDateTime()+"] --- Sending message "+notification_id+" to dispatcher....");
-                const endpoint = endpoints[balancer.pick()];
+                var endpoint = endpoints[Math.floor(Math.random()*endpoints.length)];
                 axios.post(endpoint,
                 {sendPushRequest}
                 )
@@ -443,7 +443,7 @@ router.post('/v33',async(req,res,next)=>{
                 console.log("sendPushRequest JSON");
                 console.log("%j",sendPushRequest);
                 console.log("sending message to dispatcher....");
-                const endpoint = endpoints[balancer.pick()];
+                var endpoint = endpoints[Math.floor(Math.random()*endpoints.length)];
                 axios.post(endpoint,
                 {sendPushRequest}
                 )
