bfca31f23f0ecd05c74f0b197de3606b9d30349c
Mohamed Ali <naptoon.ahmed@gmail.com>
2020-07-02 10:18:29 -0300

    Trying to solve Undefined Title Problem

--- v4/message/controllers/expandWorker.js
+++ v4/message/controllers/expandWorker.js
@@ -5,8 +5,8 @@ const con = require('../connection/DBconnection');
 const ip = require('ip');
 
 const endpoints = [
-    //'http://ec2-54-166-246-71.compute-1.amazonaws.com:8080/api/message/',
-    //'http://ec2-3-95-151-234.compute-1.amazonaws.com:8080/api/message/',
+    'http://ec2-54-166-246-71.compute-1.amazonaws.com:8080/api/message/',
+    'http://ec2-3-95-151-234.compute-1.amazonaws.com:8080/api/message/',
     'http://'+ip.address()+':8080/api/message/'
 ];
 
