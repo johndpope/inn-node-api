809abb096e751f4f9fe408b3169968500b977956
Mohamed Ali <naptoon.ahmed@gmail.com>
2020-07-02 21:38:29 -0300

    trying to solve undefined title

--- v4/message/controllers/expandWorker.js
+++ v4/message/controllers/expandWorker.js
@@ -711,8 +711,8 @@ async function selectAppConfigAndUserData(subscriber_id,app_id){
 }
 
 async function selectFromMLI(){
-    const sql = await new Promise((res,rej)=>{
-        con.query(`SELECT mli.id AS notification_id,mli.subscriber_id,mli.control_message_id,cm.app_id FROM message_log_insert mli 
+    const sql = await new Promise(async (res,rej)=>{
+     await   con.query(`SELECT mli.id AS notification_id,mli.subscriber_id,mli.control_message_id,cm.app_id FROM message_log_insert mli 
                     JOIN control_message cm ON mli.control_message_id = cm.id_control_message 
                     WHERE (cm.status = 3 OR cm.status = 4) AND 
                     mli.message_status_id = 0 
@@ -855,12 +855,13 @@ async function getSubPhone(sub_id, app_id){
 }
 
 
-function isLast(v3messages,key)
+async function isLast(v3messages,key)
 {
     if (Object.is(v3messages.length -1,key)) {
-        let recall  =  axios.post('http://'+ip.address()+':8080'+'/api/expandWorker/v33/');
+        await new Promise(resolve => setTimeout(resolve, 2000));
+       await axios.post('http://'+ip.address()+':8080/api/expandWorker/v33/');
         console.log('-------------------------[Calling The EW AGAIN..]------------------------------------------');
-        console.info('                      http://'+ip.address()+':8080/api/expandWorker/v33/         ');
+        console.log('                      http://'+ip.address()+':8080/api/expandWorker/v33/         ');
         console.log('-------------------------------------------------------------------------------------------');
     }
 }
