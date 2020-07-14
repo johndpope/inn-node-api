fa7891c968135c138029ac5c7eca03e909c31766
Mohamed Ali <naptoon.ahmed@gmail.com>
2020-07-14 15:30:11 -0300

    responses

--- v4/message/controllers/ew.js
+++ v4/message/controllers/ew.js
@@ -7,7 +7,6 @@ const SaveLog = require('../../utils/logger').expandWorkerLogging;
 const {parse, stringify} = require('flatted');
 
 const endpoints = [
-    'http://'+ip.address()+':8080/api/message/',
     'http://ec2-54-166-246-71.compute-1.amazonaws.com:8080/api/message/',
     'http://ec2-3-95-151-234.compute-1.amazonaws.com:8080/api/message/'
 ];
@@ -482,23 +481,23 @@ router.post('/v11',(req,res0,next)=>{
                         SaveLog.info("[" + getDateTime() + "][CM:][" + controlMessageId + "][NotId][" + recipient.not_id + "]" + stringify(response.data));
                     })
                     .catch(async er => {
-                        console.log("Error on sending  to Dispatcher...");
+                        console.log("[Response Error] on sending  to Dispatcher...");
                         if (er.response) {
                             // The request was made and the server responded with a status code
                             // that falls out of the range of 2xx
-                            SaveLog.error("[" + getDateTime() + "] Error on sending  to Dispatcher .. [CM:][" + controlMessageId + "][NotId][" + recipient.not_id + "] " + stringify(er.response));
+                            SaveLog.error("[" + getDateTime() + "] [Response Error]  on sending  to Dispatcher .. [CM:][" + controlMessageId + "][NotId][" + recipient.not_id + "] " + stringify(er.response));
                             console.log(er.response);
 
                         } else if (er.request) {
                             // The request was made but no response was received
                             // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
                             // http.ClientRequest in node.js
-                            SaveLog.error("[" + getDateTime() + "] Error on sending  to Dispatcher .. [CM:][" + controlMessageId + "][NotId][" + recipient.not_id + "] " + stringify(er.request));
+                            SaveLog.error("[" + getDateTime() + "] [Request Error]  on sending  to Dispatcher .. [CM:][" + controlMessageId + "][NotId][" + recipient.not_id + "] " + stringify(er.request));
                             console.log(er.request);
                         } else {
                             // Something happened in setting up the request that triggered an Error
-                            SaveLog.error("[" + getDateTime() + "] Error on sending  to Dispatcher .. [CM:][" + controlMessageId + "][NotId][" + recipient.not_id + "] " + stringify(er.message));
-                            console.log('Error', er.message);
+                            SaveLog.error("[" + getDateTime() + "] [Error] on sending  to Dispatcher .. [CM:][" + controlMessageId + "][NotId][" + recipient.not_id + "] " + stringify(er.message));
+                            console.log('[Error]', er.message);
                         }
 
 
