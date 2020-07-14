fdc1ab9d3c4943b223cdda58b1b540b9ed2e932e
Mohamed Ali <naptoon.ahmed@gmail.com>
2020-07-14 12:45:31 -0300

    changed pool connection config due to handshake error

--- v4/message/connection/config.js
+++ v4/message/connection/config.js
@@ -1,4 +1,8 @@
 let config = {
+    connectionLimit : 1000,
+    connectTimeout  : 60 * 60 * 1000,
+    acquireTimeout  : 60 * 60 * 1000,
+    timeout         : 60 * 60 * 1000,
     host    : 'ec2-34-227-160-194.compute-1.amazonaws.com',
     user    : 'inn_usr',
     password: 'cCUyzADjS5QPHVVt59w46NqSdVFrepM9',
